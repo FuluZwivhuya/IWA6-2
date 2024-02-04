6bc73a03af397bce17813ace56087b683dd087d3
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-04 21:04:41 +0200

    tax done

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const rent = 400;
+const tax = "8%";
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = "00";
+const minuteOfDay = "00";
+
+// Only change below this line
+
+if (hourOfDay == undefined && minuteOfDay == undefined) {
+  console.log(null);
+} else 
+ if (hourOfDay == "00" && minuteOfDay == "00") {
+  const taxAsDecimal = (salary * 8) / 100;                    // % is NAN, therefore I used basic maths, converted 8% to numerical value which is 8/100.
+  const startingAfterTax = salary * 1 - taxAsDecimal;
+  const balance = startingAfterTax - transport - food - rent; // Replaced start with startingaftertax
+  console.log(taxAsDecimal);//test
+  console.log(startingAfterTax);//test
+  console.log(balance.toFixed(2));
+}
